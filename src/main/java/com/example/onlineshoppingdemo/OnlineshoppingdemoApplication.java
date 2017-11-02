12d60942beb0a0ec482b6f03c6ac5ac59fc3ca6a
soumyashree1990@gmail.com <Somu@6084>
2017-11-02 15:57:02 -0700

    Initial commit

---
+++ src/main/java/com/example/onlineshoppingdemo/OnlineshoppingdemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.onlineshoppingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineshoppingdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineshoppingdemoApplication.class, args);
+	}
+}
